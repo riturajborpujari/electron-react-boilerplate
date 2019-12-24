f91881b1ec4900787d85c0b06f4af0b8a7bf00d7
Rituraj Borpujari <riturajborpujari@outlook.com>
2019-12-25 00:01:39 +0530

    BUG_FIX

--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -1,8 +1,6 @@
 const net = require('net');
 const port = process.env.PORT ? (process.env.PORT - 100) : 3000;
 
-const find = require('find-process')
-
 process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 
 const client = new net.Socket();
@@ -18,24 +16,6 @@ const tryConnection = () => client.connect({ port: port }, () => {
         electron.stdout.on("data", function (data) {
             console.log("stdout: " + data.toString());
         });
-
-        electron.on('exit', _ => {
-            find('name', 'node', 0)
-                .then((list) => {
-                    list.forEach(item => {
-                        if (item.cmd.indexOf('react-scripts') !== -1) {
-                            console.log('Found react-script process', item)
-                            console.log(process.kill(item.pid, 'SIGHUP') ?
-                                `Killed process ${item.pid}` :
-                                `Could not kill process ${item.pid}`)
-
-                        }
-                    })
-
-                }).catch((e) => {
-                    console.log(e);
-                });
-        })
     }
 }
 );
